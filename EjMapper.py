906fa898b38db8fb855f206449f89208843fa591
José Incera <jincera@users.noreply.github.com>
2024-10-01 12:40:39 -0600

    Update EjMapper.py

--- EjMapper.py
+++ EjMapper.py
@@ -14,6 +14,6 @@ for Line in sys.stdin:
 	Data = Line.strip().split(",")
 	if len(Data) == 4:
 		hora, genero, distrito, candidato = Data
-		print ("{0}\t{1}".format(genero,distrito))
+		print ("{0}\t{1}".format(candidato, 1))
 
 
